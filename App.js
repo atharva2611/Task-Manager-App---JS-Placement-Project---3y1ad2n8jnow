39cd150cf2db9d7447b24a62fb63a002884f0445
Atharva.Kashikar <atharva.kashikar.2@gmail.com>
2023-01-12 16:38:21 +0530

    Final commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 const tasks = document.querySelectorAll(".task");
 const all_status = document.querySelectorAll(".status");
+let append_task = document.querySelector("#open");
 let draggableTask = null;
 
 tasks.forEach((task) => {
@@ -7,12 +8,16 @@ tasks.forEach((task) => {
   task.addEventListener("dragend", dragEnd);
 });
 
-function dragStart() {
+function del_task (e){
+  e.target.parentElement.parentElement.style.display = "none";
+}
+
+let isDraggable;
+
+function dragStart(e) {
+  isDraggable = e.target.parentElement.classList[1];
   draggableTask = this;
-  setTimeout(() => {
-    this.style.display = "none";
-  }, 0);
-  console.log("dragStart");
+ 
 }
 
 function dragEnd() {
@@ -20,7 +25,6 @@ function dragEnd() {
   setTimeout(() => {
     this.style.display = "block";
   }, 0);
-  console.log("dragEnd");
 }
 
 all_status.forEach((status) => {
@@ -32,23 +36,23 @@ all_status.forEach((status) => {
 
 function dragOver(e) {
   e.preventDefault();
-  //   console.log("dragOver");
 }
 
 function dragEnter() {
   this.style.border = "1px dashed #ccc";
-  console.log("dragEnter");
 }
 
 function dragLeave() {
   this.style.border = "none";
-  console.log("dragLeave");
 }
 
-function dragDrop() {
-  this.style.border = "none";
-  this.appendChild(draggableTask);
-  console.log("dropped");
+function dragDrop(e) {
+  if(isDraggable === e.target.id){
+    this.style.border = "none";
+    this.appendChild(draggableTask);
+  }else{
+    alert('The drag should be in sequence.');
+  }
 }
 
 /* modal */
@@ -79,48 +83,53 @@ window.onclick = (event) => {
   }
 };
 
+
 /* create task  */
 const task_submit = document.getElementById("task-submit");
 
-task_submit.addEventListener("click", createTask);
 
 function createTask() {
-  console.log("batao bhai");
+
+  const input_name = document.getElementById("task-name").value;
+  const input_desc = document.querySelector("#task-description").value;
+
   const task_div = document.createElement("div");
-  const input_val = document.getElementById("task-name").value;
-  const txt = document.createTextNode(input_val);
-
-  task_div.appendChild(txt);
   task_div.classList.add("task");
-  task_div.setAttribute("draggable", "true");
-  /* create span */
-  const span = document.createElement("span");
-  const span_txt = document.createTextNode("\u00D7");
-  span.classList.add("close");
-  span.appendChild(span_txt);
-
-  task_div.appendChild(span);
-
-  no_status.appendChild(task_div);
-
-  span.addEventListener("click", () => {
-    span.parentElement.style.display = "none";
-  });
-  //   console.log(task_div);
-
+  task_div.draggable = "true";
   task_div.addEventListener("dragstart", dragStart);
   task_div.addEventListener("dragend", dragEnd);
 
-  document.getElementById("task_input").value = "";
-  task-form.classList.remove("active");
-  overlay.classList.remove("active");
+  const header_div = document.createElement("div");
+  header_div.setAttribute("id", "task-heading-task");
+  header_div.innerHTML = input_name;
+
+  const close_span = document.createElement("span");
+  close_span.classList.add("close");
+  close_span.onclick = del_task;
+  const span_txt = document.createTextNode("\u00D7");
+  close_span.appendChild(span_txt);
+
+  header_div.appendChild(close_span);
+
+  const desc_div = document.createElement("div");
+  desc_div.setAttribute("id", "description");
+  desc_div.innerHTML = input_desc;
+
+  task_div.appendChild(header_div);
+  task_div.appendChild(desc_div);
+
+  append_task.appendChild(task_div);
+
 }
 
 const close_btns = document.querySelectorAll(".close");
+task_submit.addEventListener("click", createTask);
+
 
 close_btns.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.parentElement.style.display = "none";
   });
 });
+
 
